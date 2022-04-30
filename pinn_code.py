2260122e4fe037effcb23eb5be23d9670164760e
William ROCHE <william.roche@student-cs.fr>
2022-04-30 18:28:36 +0200

    ajout argparse

--- pinn_code.py
+++ pinn_code.py
@@ -6,29 +6,45 @@ from time import time
 from initial import *
 from model import *
 from plot import * 
+import argparse
 #%%
 DTYPE = 'float32'
 tf.keras.backend.set_floatx(DTYPE)
 
-##Constantes à modifier (c:vitesse de l'onde,dimension:2D ou 3D,k : constante pour la gaussienne de l'onde à l'instant initial)
-c = 2
-dimension = 1
+parser = argparse.ArgumentParser()
+parser.add_argument("-c", help="choose the velocity of the wave", type = float, default=1)
+parser.add_argument("-dim", help= "dimension of the modelisation", choices=[1, 2, 3], type=int, default=1)
+parser.add_argument("-Nb", help = "number of points at the edge", type = int, default=100)
+parser.add_argument("-N0", help = "number of initial points", type = int, default=100)
+parser.add_argument("-Nr", help = "number of residual points", type = int, default=500)
+args = parser.parse_args()
+
+c = args.c
+dimension = args.dim
 w = np.pi*c*np.sqrt(2**2+3**2)
 
-tmin,tmax = 0.,1.
-xmin,xmax = 0.,1.
+tmin,tmax = 0.,1.0
+xmin,xmax = -1.,1.
+
+N_b = args.Nb
+N_0 = args.N0
+N_r = args.Nr
+
+##Constantes à modifier (c:vitesse de l'onde,dimension:2D ou 3D,k : constante pour la gaussienne de l'onde à l'instant initial)
+w = np.pi*c*np.sqrt(2**2+3**2)
+
+tmin,tmax = 0.,1.0
+xmin,xmax = -1.,1.
 
 def train():
     #Number of points
     #N_0 = 500 #Nombre de points pour la condition initiale
     #N_b = 500 #Nombre de points pour la condition aux bords
     #N_r = 10000 #Nombre de points pour le résidu (donc à l'intérieur du domaine)
-    N_b = 1000
-    N_0 = 1000
-    N_r = 100000
+    
     X_data,u_data,time_x,X_r = set_training_data(tmin,tmax,xmin,xmax,dimension,N_0,N_b,N_r)
 
-    plot_training_points(dimension,time_x)
+    #plot_training_points(dimension,time_x)
 
     bound1 = [tmin] + [xmin for _ in range(dimension)]
     bound2 = [tmax] + [xmax for _ in range(dimension)]
@@ -109,29 +125,10 @@ N = 70
 fps = 5
 tspace = np.linspace(lb[0], ub[0], N + 1)
 
-plot1d(lb,ub,N,tspace,model,fps)
-# %%
-def plot1d(lb,ub,N,tspace,model,fps):
-    ###1D Wave
-    x1space = np.linspace(lb[1], ub[1], N + 1)
-
-    T,X1 = np.meshgrid(tspace,x1space)
-    Xgrid = tf.stack([T.flatten(),X1.flatten()],axis=-1)
-
-    upred = model(tf.cast(Xgrid,DTYPE))
-    U = upred.numpy().reshape(N+1,N+1)
-    z_array = np.zeros((N+1,N+1))
-    for i in range(N):
-        z_array[:,i]= U[i]
-
-    plt.style.use('dark_background')
-
-    fig = plt.figure()
-    ax = fig.add_subplot(111)
-    ax.scatter(T,X1,c=U, marker='X', vmin=0, vmax=1)
-    ax.set_xlabel('$t$')
-    ax.set_ylabel('$x1$')
-    plt.show()
-
-plot1d(lb,ub,N,tspace,model,fps)      
+if dimension == 1 :
+    plot1d(lb,ub,N,tspace,model,fps)
+elif dimension == 2 :
+    plot2d(lb,ub,N,tspace,model,fps)
+else :
+    plot3d(lb,ub,N,tspace,model,fps)
 # %%
