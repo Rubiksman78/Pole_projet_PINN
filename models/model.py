1b5d1bc0b69d9112a036e65efa7c0aceb082d06f
Doragh Ilyess <ilyess.doragh@student-cs.fr>
2022-09-29 11:09:12 +0200

    "Voila! I have made a commit!"

--- models/model.py
+++ models/model.py
@@ -4,9 +4,10 @@ import keras.layers as kl
 from keras import models
 from physics.initial import *
 from models.kernel_upgrade import *
+import keras
 
 
-def define_net(num_inputs,num_outputs,ub,lb,n_layers = 8, n_neurons = 512):
+def define_net(num_inputs, num_outputs, ub, lb, n_layers=8, n_neurons=512):
     """
     Neural network architecture for PINN
     Input : num_inputs = dimension of input vector
@@ -16,33 +17,36 @@ def define_net(num_inputs,num_outputs,ub,lb,n_layers = 8, n_neurons = 512):
             n_neurons : number of neurons per hidden layer
     Ouput : Tensorflow model f: num_inputs -> num_outputs
     """
-    input = models.Input(shape=(num_inputs))
-    scaling_layer = tf.keras.layers.Lambda(lambda x: 2.0*(x - lb)/(ub - lb) - 1.0) #Normalisation des points en [-1,1]
-    x = scaling_layer(input) 
+    input = keras.Input(shape=(num_inputs))
+    # Normalisation des points en [-1,1]
+    scaling_layer = tf.keras.layers.Lambda(
+        lambda x: 2.0*(x - lb)/(ub - lb) - 1.0)
+    x = scaling_layer(input)
     for _ in range(n_layers):
-        x = kl.Dense(n_neurons,activation='tanh')(x)
+        x = kl.Dense(n_neurons, activation='tanh')(x)
         x = kl.BatchNormalization()(x)
-    x = kl.Dense(num_outputs,activation='linear')(x)
-    return models.Model(input,x)
+    x = kl.Dense(num_outputs, activation='linear')(x)
+    return models.Model(input, x)
 
 
 class PINN(models.Model):
     def __init__(self,
-    num_inputs,
-    num_outputs,
-    dimension,
-    ub,
-    lb,
-    c,
-    init_weight = 1,
-    bound_weight = 1,
-    res_weight = 1,
-    bound_speed_weight = 1,
-    n_layers = 6,
-    n_neurons = 64,
-    **kwargs):
+                 num_inputs,
+                 num_outputs,
+                 dimension,
+                 ub,
+                 lb,
+                 c,
+                 init_weight=1,
+                 bound_weight=1,
+                 res_weight=1,
+                 bound_speed_weight=1,
+                 n_layers=6,
+                 n_neurons=64,
+                 **kwargs):
         super().__init__(**kwargs)
-        self.model = define_net(num_inputs,num_outputs,ub,lb,n_layers,n_neurons)
+        self.model = define_net(num_inputs, num_outputs,
+                                ub, lb, n_layers, n_neurons)
         self.dimension = dimension
         self.c = c
         self.init_weight = init_weight
@@ -50,41 +54,39 @@ class PINN(models.Model):
         self.res_weight = res_weight
         self.bound_speed_weight = bound_speed_weight
 
-
-    def compile(self,opt):
+    def compile(self, opt):
         super().compile()
         self.opt = opt
 
-
-    def get_r(self,X_r):
+    def get_r(self, X_r):
         """
         Compute the residual of the PDE
         Input : X_r = residual points sampled to compute the residual
         Ouput : values of the residual over X_r
         """
-        #Compute the first order gradient 
+        # Compute the first order gradient
         with tf.GradientTape(persistent=True) as tape2:
             X_r_unstacked = tf.unstack(X_r, axis=1)
             tape2.watch(X_r_unstacked)
-            with tf.GradientTape(persistent=True) as tape1: 
+            with tf.GradientTape(persistent=True) as tape1:
                 x_stacked = tf.stack(X_r_unstacked, axis=1)
                 tape1.watch(x_stacked)
-                u = self.model(x_stacked) 
-            gradient_x = tape1.gradient(u,x_stacked)
-            gradient_x_unstacked = tf.unstack(gradient_x,axis=1)
+                u = self.model(x_stacked)
+            gradient_x = tape1.gradient(u, x_stacked)
+            gradient_x_unstacked = tf.unstack(gradient_x, axis=1)
 
-        #Compute second order gradient
+        # Compute second order gradient
         d2f_dx2 = []
-        for df_dxi,xi in zip(gradient_x_unstacked, X_r_unstacked):
+        for df_dxi, xi in zip(gradient_x_unstacked, X_r_unstacked):
             d2f_dx2.append(tape2.gradient(df_dxi, xi))
         u_xx = tf.stack(d2f_dx2, axis=1)
-        u_tt = u_xx[:,0]
-        u_xx = tf.reduce_sum([u_xx[:,i] for i in range(1,self.dimension+1)],axis=0)
+        u_tt = u_xx[:, 0]
+        u_xx = tf.reduce_sum([u_xx[:, i]
+                              for i in range(1, self.dimension+1)], axis=0)
 
-        return residual(X_r[:,0],X_r[:,1],gradient_x[:,0],u_tt,u_xx,self.c) 
+        return residual(X_r[:, 0], X_r[:, 1], gradient_x[:, 0], u_tt, u_xx, self.c)
 
-
-    def compute_loss(self,X_r,X_data,u_data,use_r = True):
+    def compute_loss(self, X_r, X_data, u_data, use_r=True):
         """
         Compute the residual, boundary and initial losses
         Input : X_r = residual points
@@ -93,9 +95,9 @@ class PINN(models.Model):
         Output : losses on initial, boundary, boundary speed condition and mean residual 
         """
         if use_r:
-            #Residual loss 
+            # Residual loss
             res = self.get_r(X_r)
-            phi_r = tf.reduce_mean(tf.square(res)) 
+            phi_r = tf.reduce_mean(tf.square(res))
         else:
             phi_r = 0
         loss_i = 0
@@ -103,38 +105,39 @@ class PINN(models.Model):
         loss_b2 = 0
         for i in range(len(X_data)):
             if i == 1:
-                #Loss boundary condition
-                u_pred = self.model(X_data[i]) 
+                # Loss boundary condition
+                u_pred = self.model(X_data[i])
                 loss_b1 += tf.reduce_mean(tf.square(u_data[i]-u_pred))
             elif i == 0:
-                #Loss initial condition
-                u_pred = self.model(X_data[i]) 
-                loss_i += tf.reduce_mean(tf.square(u_data[i]-u_pred)) 
+                # Loss initial condition
+                u_pred = self.model(X_data[i])
+                loss_i += tf.reduce_mean(tf.square(u_data[i]-u_pred))
             else:
-                #Loss initial speed condition
-                with tf.GradientTape() as tape: 
-                    t = X_data[i][:,0]
+                # Loss initial speed condition
+                with tf.GradientTape() as tape:
+                    t = X_data[i][:, 0]
                     tape.watch(t)
-                    x = [X_data[i][:,j] for j in range(1,self.dimension+1)]
-                    u_pred = self.model(tf.stack([t]+[xi for xi in x],axis=1)) 
-                    v_pred = tape.gradient(u_pred,t) 
-                loss_b2 += tf.reduce_mean(tf.square(u_data[i]-tf.expand_dims(v_pred,axis=-1)))
-        return loss_i,loss_b1,loss_b2,phi_r
+                    x = [X_data[i][:, j] for j in range(1, self.dimension+1)]
+                    u_pred = self.model(tf.stack([t]+[xi for xi in x], axis=1))
+                    v_pred = tape.gradient(u_pred, t)
+                loss_b2 += tf.reduce_mean(
+                    tf.square(u_data[i]-tf.expand_dims(v_pred, axis=-1)))
+        return loss_i, loss_b1, loss_b2, phi_r
 
-
-    def train_step(self,X_r,X_data,u_data,i,use_kernel = True):
+    def train_step(self, X_r, X_data, u_data, i, use_kernel=True):
         with tf.GradientTape() as tape:
-            #Computing lambda coefficients
-            if use_kernel and (i+1) % 500 ==0:
-                Ju = compute_Ju(tf.concat([X_data[0],X_data[1]],axis=0),self.model)
-                Jr = compute_Jr(X_r,self.model,self.c,self.dimension)
-                Jut = compute_Ju(X_data[2],self.model)
-                Kuu = compute_Ki(tf.concat([X_data[0],X_data[1]],axis=0),Ju)
-                Krr = compute_Ki(X_r,Jr)
-                Kut = compute_Ki(X_data[2],Jut)
-                lambda_b = compute_eigen(Kuu,Krr,Kut,name='Kuu')
-                lambda_r = compute_eigen(Kuu,Krr,Kut,name='Krr')
-                lambda_bv = compute_eigen(Kuu,Krr,Kut,name='Kut')
+            # Computing lambda coefficients
+            if use_kernel and (i+1) % 500 == 0:
+                Ju = compute_Ju(
+                    tf.concat([X_data[0], X_data[1]], axis=0), self.model)
+                Jr = compute_Jr(X_r, self.model, self.c, self.dimension)
+                Jut = compute_Ju(X_data[2], self.model)
+                Kuu = compute_Ki(tf.concat([X_data[0], X_data[1]], axis=0), Ju)
+                Krr = compute_Ki(X_r, Jr)
+                Kut = compute_Ki(X_data[2], Jut)
+                lambda_b = compute_eigen(Kuu, Krr, Kut, name='Kuu')
+                lambda_r = compute_eigen(Kuu, Krr, Kut, name='Krr')
+                lambda_bv = compute_eigen(Kuu, Krr, Kut, name='Kut')
                 self.init_weight = lambda_b
                 self.bound_weight = lambda_b
                 self.res_weight = lambda_r
@@ -143,21 +146,21 @@ class PINN(models.Model):
             lambda_b = self.init_weight
             lambda_r = self.res_weight
             lambda_bv = self.bound_speed_weight
-            #Summing losses with their lambda weights
-            loss_i,loss_b1,loss_b2,loss_r = self.compute_loss(X_r,X_data,u_data) #On calcule le coût par rapport aux données
+            # Summing losses with their lambda weights
+            loss_i, loss_b1, loss_b2, loss_r = self.compute_loss(
+                X_r, X_data, u_data)  # On calcule le coût par rapport aux données
             loss = self.init_weight * loss_i + \
                 self.bound_weight * loss_b1 + \
                 self.bound_speed_weight * loss_b2 + \
                 self.res_weight * loss_r
-        #Gradient optimization 
-        grads = tape.gradient(loss,self.model.trainable_variables) 
-        self.opt.apply_gradients(zip(grads,self.model.trainable_variables)) 
-        return loss_i,loss_b1,loss_b2,loss_r,lambda_b,lambda_bv,lambda_r
+        # Gradient optimization
+        grads = tape.gradient(loss, self.model.trainable_variables)
+        self.opt.apply_gradients(zip(grads, self.model.trainable_variables))
+        return loss_i, loss_b1, loss_b2, loss_r, lambda_b, lambda_bv, lambda_r
 
-
-    def test_step(self,x,f_real):
-        #To validate the model with the real prediction
+    def test_step(self, x, f_real):
+        # To validate the model with the real prediction
         y_real = f_real(x)
         y_pred = self.model(x)
-        loss = tf.keras.losses.MeanSquaredError()(y_real,y_pred)
+        loss = tf.keras.losses.MeanSquaredError()(y_real, y_pred)
         return loss
--- pinn_code.py
+++ pinn_code.py
@@ -1,124 +1,106 @@
-#%%
+# %%
 import numpy as np
 import tensorflow as tf
 from tensorflow import keras
 from time import time
 from physics.initial import *
 from models.model import *
-from utils.plot import * 
+from utils.plot import *
 from physics.equation import *
 from config import *
 from tqdm import tqdm
-#%%
+# %%
 DTYPE = 'float32'
 tf.keras.backend.set_floatx(DTYPE)
 
-#Training loop
-def train(epochs,pinn,X_r,X_data,u_data,f_real,N,dimension,batch_size,render_bar=True,val_ratio = 0.1,begin_from = 80000):
+# Training loop
+# Nb de boucle du NN, modele, Valeurs du dataset, CI de X, ..., fonction reelle, N,dim,
+# reduit le calcul par petit groupe, barre de progression, ratio de data utilisé
+# pour validation/test/entrainement
+
+
+def train(epochs, pinn, X_r, X_data, u_data, f_real, N, dimension, batch_size, render_bar=True, val_ratio=0.1, begin_from=80000):
     losses = []
     val_losses = []
     t0 = time()
 
-    #Setting validation dataset size
+    # Setting validation dataset size
     total_size = tf.shape(X_r)[0].numpy()
     val_size = (val_ratio * total_size).astype(int)
     if batch_size > total_size - val_size:
         print(total_size - val_size)
         raise ValueError("Batch size > train size")
-    
-    #Setting train and validation data
-    X_data_test = tf.concat(X_data,axis=0)[-val_size:]
-    X_test = tf.concat([X_r[-val_size:],X_data_test],axis=0)
-    X_r,X_data,u_data = X_r[:-val_size],[x[:-val_size] for x in X_data],[x[:-val_size] for x in u_data]
+
+    # Setting train and validation data
+    X_data_test = tf.concat(X_data, axis=0)[-val_size:]
+    X_test = tf.concat([X_r[-val_size:], X_data_test], axis=0)
+    X_r, X_data, u_data = X_r[:-val_size], [x[:-val_size]
+                                            for x in X_data], [x[:-val_size] for x in u_data]
     num_steps = (np.ceil(tf.shape(X_r)[0].numpy())/batch_size).astype(int)
 
-    val_loss = pinn.test_step(X_test,f_real)
+    val_loss = pinn.test_step(X_test, f_real)
     val_losses.append(val_loss)
 
     if render_bar:
-            progress_bar = tqdm(range(epochs+1))
+        progress_bar = tqdm(range(epochs+1))
     else:
         progress_bar = range(epochs+1)
-    
-    #Main training loop over epochs
+
+    # Main training loop over epochs
     for i in progress_bar:
         loss = 0
-        #Loop over all batches
+        # Loop over all batches
         for j in range(num_steps):
             idx_i = j*batch_size
             idx_e = (j+1)*batch_size
-            X_rj,X_dataj,u_dataj = X_r[idx_i:idx_e],tf.constant(np.array(X_data)[:,idx_i:idx_e]),\
-                tf.constant(np.array(u_data)[:,idx_i:idx_e])
-            loss_i,loss_b1,loss_b2,loss_r,lambda_b,lambda_bv,lambda_r = \
-                pinn.train_step(X_rj,X_dataj,u_dataj,i) #Calling train step on batch
-            loss_j = loss_i + loss_b1 + loss_b2 + loss_r 
+            X_rj, X_dataj, u_dataj = X_r[idx_i:idx_e], tf.constant(np.array(X_data)[:, idx_i:idx_e]),\
+                tf.constant(np.array(u_data)[:, idx_i:idx_e])
+            loss_i, loss_b1, loss_b2, loss_r, lambda_b, lambda_bv, lambda_r = \
+                pinn.train_step(X_rj, X_dataj, u_dataj,
+                                i)  # Calling train step on batch
+            loss_j = loss_i + loss_b1 + loss_b2 + loss_r
             loss += loss_j
         loss = loss / num_steps
         losses.append(loss)
-        #Rendering training metrics 
+        # Rendering training metrics
         if render_bar:
             progress_bar.set_description(f"Epoch {i}: Loss= {loss}")
             if (i+1) % 50 == 0:
-                val_loss = pinn.test_step(X_test,f_real)
+                val_loss = pinn.test_step(X_test, f_real)
                 val_losses.append(val_loss)
                 #print(f"Epoch {i}: val_loss : {val_loss}")
-        
+
         if (i+1) % 10000 == 0:
-                print(f'It {i}: residual_loss = {loss_r}\
+            print(f'It {i}: residual_loss = {loss_r}\
                     | initial_loss = {loss_i}\
                     | boundary_loss_x = {loss_b1}\
                     | boundary_loss_v = {loss_b2}\
                     | lambda_b = {lambda_b}\
                     | lambda_bv = {lambda_bv}\
                     | lambda_r = {lambda_r}')
-        
-        if (i+1) % 1000 ==0:
-            pinn.model.save_weights('results/pinn200.h5') #Change name file for another train
-        if (i+1) % 2000 ==0:
-            if dimension == 1:
-                plot1dgrid(lb,ub,N,pinn.model,i+begin_from)
-        if (i+1) % 50 ==0:
-            plot_curve(i,losses,val_losses,'plot2.png')
-    print('\nComputation time: {} seconds'.format(time()-t0))
-    return losses,val_losses
 
-#Multiple trainings for different number of points
+        if (i+1) % 1000 == 0:
+            # Change name file for another train
+            pinn.model.save_weights('results/pinn200.h5')
+        if (i+1) % 2000 == 0:
+            if dimension == 1:
+                plot1dgrid(lb, ub, N, pinn.model, i+begin_from)
+        if (i+1) % 50 == 0:
+            plot_curve(i, losses, val_losses, 'plot2.png')
+    print('\nComputation time: {} seconds'.format(time()-t0))
+    return losses, val_losses
+
+# Multiple trainings for different number of points
+
+
 def multi_train():
     times = []
     points = np.concatenate(
-        (np.arange(0,100,10),np.arange(100,1050,50)),axis=0
-        )
+        (np.arange(0, 100, 10), np.arange(100, 1050, 50)), axis=0
+    )
     for N_0 in points:
         config = define_config()
-        c,a,dimension,tmin,tmax,xmin,xmax,N_b,N_r,N_0,lr,epochs = \
-                config['c'],\
-                config['a'],\
-                config['dimension'],\
-                config['tmin'],\
-                config['tmax'],\
-                config['xmin'],\
-                config['xmax'],\
-                N_0,\
-                N_0,\
-                N_0,\
-                config['learning_rate'],\
-                config['epochs']
-        X_data,u_data,time_x,X_r = set_training_data(tmin,tmax,xmin,xmax,dimension,N_0,N_b,N_r)
-        bound1 = [tmin] + [xmin for _ in range(dimension)]
-        bound2 = [tmax] + [xmax for _ in range(dimension)]
-        lb,ub = tf.constant(bound1,dtype=DTYPE),tf.constant(bound2,dtype=DTYPE)
-        opt = keras.optimizers.Adam(learning_rate=lr)
-        pinn = PINN(dimension+1,1,dimension,ub,lb,c)
-        pinn.compile(opt)
-        t_0 = time()
-        train(epochs,pinn,X_r,X_data,u_data,true_u,N=100,dimension=dimension,batch_size=32)
-        times.append(time()-t_0)
-    return times
-
-# %%
-if __name__ == '__main__':
-    config = define_config()
-    c,a,dimension,tmin,tmax,xmin,xmax,N_b,N_r,N_0,lr,epochs = \
+        c, a, dimension, tmin, tmax, xmin, xmax, N_b, N_r, N_0, lr, epochs = \
             config['c'],\
             config['a'],\
             config['dimension'],\
@@ -126,33 +108,67 @@ if __name__ == '__main__':
             config['tmax'],\
             config['xmin'],\
             config['xmax'],\
-            config['N_b'],\
-            config['N_r'],\
-            config['N_0'],\
+            N_0,\
+            N_0,\
+            N_0,\
             config['learning_rate'],\
             config['epochs']
-    X_data,u_data,time_x,X_r = set_training_data(tmin,tmax,xmin,xmax,dimension,N_0,N_b,N_r)
+        X_data, u_data, time_x, X_r = set_training_data(
+            tmin, tmax, xmin, xmax, dimension, N_0, N_b, N_r)
+        bound1 = [tmin] + [xmin for _ in range(dimension)]
+        bound2 = [tmax] + [xmax for _ in range(dimension)]
+        lb, ub = tf.constant(bound1, dtype=DTYPE), tf.constant(
+            bound2, dtype=DTYPE)
+        opt = keras.optimizers.Adam(learning_rate=lr)
+        pinn = PINN(dimension+1, 1, dimension, ub, lb, c)
+        pinn.compile(opt)
+        t_0 = time()
+        train(epochs, pinn, X_r, X_data, u_data, true_u,
+              N=100, dimension=dimension, batch_size=32)
+        times.append(time()-t_0)
+    return times
 
-    plot_training_points(dimension,time_x)
+
+# %%
+if __name__ == '__main__':
+    config = define_config()
+    c, a, dimension, tmin, tmax, xmin, xmax, N_b, N_r, N_0, lr, epochs = \
+        config['c'],\
+        config['a'],\
+        config['dimension'],\
+        config['tmin'],\
+        config['tmax'],\
+        config['xmin'],\
+        config['xmax'],\
+        config['N_b'],\
+        config['N_r'],\
+        config['N_0'],\
+        config['learning_rate'],\
+        config['epochs']
+    X_data, u_data, time_x, X_r = set_training_data(
+        tmin, tmax, xmin, xmax, dimension, N_0, N_b, N_r)
+
+    plot_training_points(dimension, time_x)
 
     bound1 = [tmin] + [xmin for _ in range(dimension)]
     bound2 = [tmax] + [xmax for _ in range(dimension)]
-    lb,ub = tf.constant(bound1,dtype=DTYPE),tf.constant(bound2,dtype=DTYPE)
-    plot1dgrid_real(lb,ub,200,lambda x: true_u(x,a,c),999999)
+    lb, ub = tf.constant(bound1, dtype=DTYPE), tf.constant(bound2, dtype=DTYPE)
+    plot1dgrid_real(lb, ub, 200, lambda x: true_u(x, a, c), 999999)
     opt = keras.optimizers.Adam(learning_rate=lr)
     hist = []
-    pinn = PINN(dimension+1,1,dimension,ub,lb,c)
+    pinn = PINN(dimension+1, 1, dimension, ub, lb, c)
     pinn.compile(opt)
     pinn.model.load_weights('results/pinn100.h5')
-    batch_size_max = int(0.9*N_b) #30% of train dataset
-    train(epochs,pinn,X_r,X_data,u_data,true_u,N=100,dimension=dimension,batch_size=batch_size_max)
+    batch_size_max = int(0.9*N_b)  # 30% of train dataset
+    train(epochs, pinn, X_r, X_data, u_data, true_u, N=100,
+          dimension=dimension, batch_size=batch_size_max)
 
-    #Test
+    # Test
     model = pinn.model
     N = 70
     fps = 5
     tspace = np.linspace(lb[0], ub[0], N + 1)
-    plot1d(lb,ub,N,tspace,model,fps)
+    plot1d(lb, ub, N, tspace, model, fps)
     N = 100
     tspace = np.linspace(lb[0], ub[0], N + 1)
-    plot1dgrid(lb,ub,N,model,0)      
+    plot1dgrid(lb, ub, N, model, 0)
